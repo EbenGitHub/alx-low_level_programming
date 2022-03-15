58449dbcc98f9bef7f0bd8e6c9cd70aa605091a0
Abenezer Eshetie <abenezergoo@gmail.com>
2022-03-15 08:16:30 -0700

    messase

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @c: int whose last digit will be printed
+ * Return: c on success
+ */
+int print_last_digit(int c)
+{
+	c %= 10;
+	if (c < 0)
+	{
+		c = -c;
+	}
+	_putchar(c + '0');
+	return (c);
+}
