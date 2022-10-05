2498b79fa7155fa010b5f152ae80f3068f0d55cb
Eben@ysh <77492217+EbenGitHub@users.noreply.github.com>
2022-10-05 17:42:08 +0300

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,7 +3,7 @@
 
 /**
  * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
+ * integers using the Jump search algorithm  
  *
  * @array: input array
  * @size: size of the array
