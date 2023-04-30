35da552e4fa09d43bdc6caf20bd4b91793798683
Ebenezer Eshetie <77492217+EbenGitHub@users.noreply.github.com>
2023-04-30 19:11:29 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 
 /**
  * main - write to stdout
- * Return: Always 0
+ * Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
